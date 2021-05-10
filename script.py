f3608b4b871ccc29e9f137285fd2a862bacd78e5
gaurav1620 <gauravak007@gmail.com>
2021-05-10 16:26:42 +0530

    adding comments and a few changes

--- script.py
+++ script.py
@@ -20,24 +20,58 @@ import time
 from prettytable import PrettyTable
 from playsound import playsound
 
+# Code to clear screen 
+from os import system, name
+from time import sleep
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+
 # Script starts here 
 
+# Do not change headers
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-res = requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/states', headers=headers)
+res = 1
+
+#Fetch a list of states
+try :
+    res = requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/states', headers=headers)
+except :
+    # If request fails, stop executing the script
+    print("Check your internet connection and try again !")
+    exit()
+
+# Load the state data
 states =res.json()['states']
+
+# Show a list of states along with index to user
 print('ID : Name of state')
 for i in states:
     print(str(i['state_id']) + ' : '+ str(i['state_name']))
 
+# ask the user to enter the index of state he wants
 state_id = input('Enter the serial number of your state : ')
 
-res = requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/' + state_id
-                   , headers=headers)
+#Fetch a list of districts in that state
+try :
+    res = requests.get('https://cdn-api.co-vin.in/api/v2/admin/location/districts/' + state_id
+                       , headers=headers)
+except :
+    # If request fails, stop executing the script
+    print("Check your internet connection and try again !")
+    exit()
+
+# Load the districts data
 districts = res.json()['districts']
 
+# Show a list of districts to the users
 for i in range(len(districts)):
     print(str(i+1) + ' : ' +districts[i]['district_name'])
 
+# Ask the user to enter the district he is in
 district_id = districts[int(input('Enter the serial number of your district : ')) - 1]['district_id']
 
 print('****************************************************')
@@ -46,10 +80,13 @@ month = input('Enter the current month in number, eg 5 for May : ')
 print('****************************************************')
 date = input('Enter the date of the month that you want to book : ')
 
+# append neccessary zeros before single digits
 if len(str(date)) == 1:
     month = '0' + date
 if len(str(month)) == 1:
     month = '0' + month
+
+# Input users age group
 print('What age group you belong to : ')
 print('1. 18-44')
 print('2. 45+')
@@ -59,16 +96,21 @@ age_group = 2 if age_group == 1 else 1
 
 show_all_info = int(input('Do you want to display info for just your age group(press 1) or all age groups(press 2) : ')) -1
 
-
+def yes_or_no(inp):
+    if inp:
+        return "YESSSS"
+    else :
+        return "NO"
 while 1:
     uri = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id='+ str(district_id) + '&date='+ str(date) + '-'+ str(month) +'-2021'
     #print(uri)
     res = requests.get(uri, headers = headers)
     if res.status_code != 200:
-
-        print(uri)
+        #print(uri)
+        print("Failed to fetch details !")
         print("Please check your Internet connectivity. If the script does not work for you email me the screenshot on gauravak007@gmail.com")
-        break;
+        continue
+
     centers = res.json()['centers']
 
     table = PrettyTable()
@@ -77,14 +119,18 @@ while 1:
     play_sound = 0    
 
     for i in centers:
-        vaccine_above_18 =  "YESSSS" if (i['sessions'][0]['available_capacity'] > 0 and i['sessions'][0]['min_age_limit'] == 18 ) else "No"
-        vaccine_above_45 =  "YESSSS" if (i['sessions'][0]['available_capacity'] > 0 and i['sessions'][0]['min_age_limit'] == 45 ) else "No"
-        if play_sound == 0 and (((vaccine_above_18 == "YESSSS") and (age_group == 2)) or ((vaccine_above_45 == "YESSSS") and (age_group == 1))):
+
+        min_age_limit = i['sessions'][0]['min_age_limit']
+        available_capacity = i['sessions'][0]['available_capacity']
+
+        vaccine_above_18 = ( available_capacity > 0 and min_age_limit == 18 )
+        vaccine_above_45 = ( available_capacity > 0 and min_age_limit == 45 ) 
+        if play_sound == 0 and ((vaccine_above_18  and (age_group == 2)) or (vaccine_above_45 and (age_group == 1))):
             play_sound = 1
         if(i['sessions'][0]['min_age_limit'] == 18 and age_group == 2) or show_all_info:
-            table.add_row([i['name'], i['sessions'][0]['available_capacity'],vaccine_above_18 ,vaccine_above_45, i['sessions'][0]['min_age_limit'] ])
+            table.add_row([i['name'], available_capacity, yes_or_no(vaccine_above_18), yes_or_no(vaccine_above_45), min_age_limit])
         if(i['sessions'][0]['min_age_limit'] == 45 and age_group == 1) or show_all_info:
-            table.add_row([i['name'], i['sessions'][0]['available_capacity'],vaccine_above_18 ,vaccine_above_45, i['sessions'][0]['min_age_limit'] ])
+            table.add_row([i['name'], available_capacity, yes_or_no(vaccine_above_18), yes_or_no(vaccine_above_45), min_age_limit])
 
 
     if (sound == 1) and (play_sound == 1):
@@ -93,6 +139,7 @@ while 1:
     if sound:
         playsound('beep.mp3')
     time.sleep(0.5)
+    clear()
     print(table)
     #print(str(i['name']) + ' has '+ str(i['sessions'][0]['available_capacity']) + ' with minimum age limit of '+ str(i['sessions'][0]['min_age_limit']))
 
